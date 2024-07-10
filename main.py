8c90082c2a68314b46c1a4340aa528c43c179b10
Ramanpreet Kaur <32161711+ramanrupal@users.noreply.github.com>
2024-07-10 13:04:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def to_upper(name):
-  return name.upper()
+    return name.upper()
+
 
 def say_hello(name):
-  print(f'Hello, {name}')
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-if __name__ == '__main__';
-name = "raman"
-say_hello(name)
-up = to_upper(name)
-print(up)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    name = 'TrainWithShubham'
+    say_hello(name)
+    up = to_upper(name)
+    print(up)
